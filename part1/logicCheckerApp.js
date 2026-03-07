fcc37afc84c16fd22b05ad38ac3085432571a1a6
Aman kumar <ajha00821@gmail.com>
2026-03-07 11:48:45 +0530

    Add logic checker app for Timmy's status

---
+++ part1/logicCheckerApp.js
@@ -0,0 +1,22 @@
+// let truthyOrFalsy = "Aman";
+// console.log(Boolean(truthyOrFalsy));
+
+let hasDeveloperJob = true;
+if (hasDeveloperJob) {
+    console.log("Timmy is employed as a developer.");
+}
+
+let isTimmyAGamer = false;
+if (isTimmyAGamer) {
+    console.log("Timmy loves to play World of Warcraft.");
+    // it will not print on you console becasue condition does not match
+}
+
+let timmyAge = 18;
+if (timmyAge >= 16) {
+    console.log("Timmy is old enough to drive.");
+}
+else {
+    console.log("Timmy is not old enough to drive.");
+    // to try it just change the value less then 16 in variable timmyAge = 16;
+}
