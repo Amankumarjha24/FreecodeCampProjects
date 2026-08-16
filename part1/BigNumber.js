e6d914a58e6053384fdff3b39fb46dd39381ecdc
Aman kumar <ajha00821@gmail.com>
2026-08-16 18:14:39 +0530

    Refactor largestOfAll function
    Refactor largestOfAll function to improve readability and variable naming.

--- part1/BigNumber.js
+++ part1/BigNumber.js
@@ -1,21 +1,15 @@
-function largestOfAll(arr) {
-  let result = [];
-  
-  for (let i = 0; i < arr.length; i++) {
-   
-    let max = arr[i][0];
-    
-  
-    for (let j = 1; j < arr[i].length; j++) {
-      if (arr[i][j] > max) {
-        max = arr[i][j];
-      }
+function largestOfAll(array) {
+    let bigNumber = []
+    for(let firstArray = 0; firstArray < array.length; firstArray++) {
+        let maximum = array[firstArray][0] // 0 first value of first array
+        for(let eachValueOfChosedArray = 1; eachValueOfChosedArray < array[firstArray].length; eachValueOfChosedArray++) {
+            if(array[firstArray][eachValueOfChosedArray] > maximum) {
+                maximum = array[firstArray][eachValueOfChosedArray]
+            }
+        }
+        bigNumber.push(maximum)
     }
-    
-    result.push(max);
-  }
-  
-  return result;
+    return bigNumber
 }
 
 console.log(largestOfAll([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
